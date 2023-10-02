544461c1293f380a8c2af01e18debde644d6ba8b
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-02 16:34:41 +0530

    added form in store modal dialouge

--- components/modals/store-modal.js
+++ components/modals/store-modal.js
@@ -1,14 +1,59 @@
 "use client";
-
+import * as z from "zod";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Modal } from "../ui/Modal";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formStruct = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const form = useForm({
+    resolver: zodResolver(formStruct),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
 
   return (
     <Modal title="Create Store" description="Add a new Category to manage product and categories" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-      Future Create Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
