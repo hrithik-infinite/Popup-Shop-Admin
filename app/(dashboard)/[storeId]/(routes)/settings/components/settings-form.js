07d1da0b461aac294a0607ff3447f7d7335dab51
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-08 00:12:42 +0530

    completed form layout for settings

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.js
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.js
@@ -1,9 +1,30 @@
 "use client";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 export const SettingsForm = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="flex items-center justify-between">
@@ -12,6 +33,29 @@ export const SettingsForm = ({ initialData }) => {
           <Trash className="h-4 w-4"></Trash>
         </Button>
       </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
       <Separator />
     </>
   );
