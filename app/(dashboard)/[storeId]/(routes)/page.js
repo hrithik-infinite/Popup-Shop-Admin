80fc4aa04aca4ed16a21eb90f051dcf763eb3858
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-07 10:46:52 +0530

    dashboard basic setup complete

--- app/(dashboard)/[storeId]/(routes)/page.js
+++ app/(dashboard)/[storeId]/(routes)/page.js
@@ -1,4 +1,11 @@
-const DashboardPage = () => {
-  return <div>dashboardddddddddddddddddddddddddd</div>;
+import prismadb from "@/lib/prismadb";
+
+const DashboardPage = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>{store?.name}</div>;
 };
 export default DashboardPage;
