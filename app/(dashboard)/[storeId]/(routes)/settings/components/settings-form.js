06388cc711c66718f9d9b810cd4a9585adeb75fb
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-07 23:57:21 +0530

    settings form basic structure

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.js
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.js
@@ -1,9 +1,18 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Trash } from "lucide-react";
 export const SettingsForm = ({ initialData }) => {
   return (
-    <div className="flex items-center justify-between">
-      <Heading title="Settings" description="Manage store prefernces" />
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store prefernces" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <Trash className="h-4 w-4"></Trash>
+        </Button>
+      </div>
+      <Separator />
+    </>
   );
 };
