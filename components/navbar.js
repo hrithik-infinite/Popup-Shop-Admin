31e96437e7810e8c3a8dc654a9f70399f9669ca4
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-07 11:56:21 +0530

    added store switch functionality

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,25 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { MainNav } from "@/components/main-nav";
-const NavBar = () => {
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
+import { redirect } from "next/navigation";
+
+const NavBar = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <div
       className="border-b 
         ">
       <div className="flex h-16 items-center px-4">
-        <div>Store Switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
