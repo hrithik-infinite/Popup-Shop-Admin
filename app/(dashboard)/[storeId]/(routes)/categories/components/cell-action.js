a0090e7bfe6621882b4f7d44dda544345194dedf
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-14 11:58:49 +0530

    fixed cell actions

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.js
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.js
@@ -22,17 +22,17 @@ export const CellAction = ({ data }) => {
   const [loading, setLoading] = useState(false);
   const onCopy = (id) => {
     navigator.clipboard.writeText(id);
-    toast.success("BillBoard ID copied!");
+    toast.success("Category ID copied!");
   };
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
       router.refresh();
-      toast.success("BillBoard Deleted!");
+      toast.success("Category Deleted!");
     } catch (error) {
       toast.error(
-        "Make sure you remove all categories using this billboard first. ",
+        "Make sure you remove all Products",
       );
     } finally {
       setLoading(false);
@@ -58,7 +58,7 @@ export const CellAction = ({ data }) => {
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
