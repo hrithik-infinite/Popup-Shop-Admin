42a81eadcdf255b41c662f7408eab56a6c05d835
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-14 13:52:57 +0530

    fixed sizes addition to main nav

--- components/main-nav.js
+++ components/main-nav.js
@@ -18,6 +18,11 @@ export function MainNav({ className, ...props }) {
       active: pathName === `/${params.storeId}/billboards`,
     },
     {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathName === `/${params.storeId}/categories`,
+    },
+    {
       href: `/${params.storeId}/sizes`,
       label: "Sizes",
       active: pathName === `/${params.storeId}/sizes`,
