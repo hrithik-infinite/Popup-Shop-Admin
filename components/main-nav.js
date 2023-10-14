2df852b8b2b872217c795efc8a5ef7831fff8487
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-14 13:51:08 +0530

    added sizes route on main navigation

--- components/main-nav.js
+++ components/main-nav.js
@@ -18,9 +18,9 @@ export function MainNav({ className, ...props }) {
       active: pathName === `/${params.storeId}/billboards`,
     },
     {
-      href: `/${params.storeId}/categories`,
-      label: "Categories",
-      active: pathName === `/${params.storeId}/categories`,
+      href: `/${params.storeId}/sizes`,
+      label: "Sizes",
+      active: pathName === `/${params.storeId}/sizes`,
     },
     {
       href: `/${params.storeId}/settings`,
