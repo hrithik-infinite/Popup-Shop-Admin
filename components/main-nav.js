b67c273b7ff0e058478b9d35c64096e4f4a56e4f
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-14 23:52:23 +0530

    added products in main nav

--- components/main-nav.js
+++ components/main-nav.js
@@ -33,6 +33,11 @@ export function MainNav({ className, ...props }) {
       active: pathName === `/${params.storeId}/colors`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathName === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathName === `/${params.storeId}/settings`,
