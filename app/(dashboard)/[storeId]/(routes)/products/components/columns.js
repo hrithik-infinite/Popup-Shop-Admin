91c05bc44e6578d93f7247e2c184b966c5d0760b
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-15 12:58:16 +0530

    copied bill board entity for products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.js
@@ -0,0 +1,17 @@
+"use client";
+import { CellAction } from "./cell-action";
+export const columns = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+    header: "Action",
+  },
+];
