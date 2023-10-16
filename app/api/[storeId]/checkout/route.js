5f6671c78cf42179acaabe820204b59841a2c901
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-10-16 21:34:22 +0530

    fix for cors

--- app/api/[storeId]/checkout/route.js
+++ app/api/[storeId]/checkout/route.js
@@ -4,7 +4,7 @@ import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://popup-shop-store.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
